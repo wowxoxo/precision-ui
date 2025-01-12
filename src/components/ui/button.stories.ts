739b4b9c46cedb078ee4d3ccc5f0991a8af7eacb
Tsivinsky Petr <creatsiv@gmail.com>
2025-01-12 13:24:45 +0300

    feat: add storybook

---
+++ src/components/ui/button.stories.ts
@@ -0,0 +1,92 @@
+import { Button, ButtonProps } from '@/components/ui/button' // Adjust the import path as necessary
+import { Meta, StoryObj } from '@storybook/react'
+
+import React from 'react'
+
+const meta: Meta<ButtonProps> = {
+  title: 'Components/Button',
+  component: Button,
+  argTypes: {
+    variant: {
+      options: [
+        'default',
+        'destructive',
+        'outline',
+        'secondary',
+        'ghost',
+        'link',
+      ],
+      control: { type: 'select' },
+    },
+    size: {
+      options: ['default', 'sm', 'lg', 'icon'],
+      control: { type: 'select' },
+    },
+  },
+}
+
+export default meta
+
+type Story = StoryObj<ButtonProps>
+
+export const Default: Story = {
+  args: {
+    children: 'Default Button',
+  },
+}
+
+export const Destructive: Story = {
+  args: {
+    variant: 'destructive',
+    children: 'Destructive Button',
+  },
+}
+
+export const Outline: Story = {
+  args: {
+    variant: 'outline',
+    children: 'Outline Button',
+  },
+}
+
+export const Secondary: Story = {
+  args: {
+    variant: 'secondary',
+    children: 'Secondary Button',
+  },
+}
+
+export const Ghost: Story = {
+  args: {
+    variant: 'ghost',
+    children: 'Ghost Button',
+  },
+}
+
+export const Link: Story = {
+  args: {
+    variant: 'link',
+    children: 'Link Button',
+  },
+}
+
+export const Small: Story = {
+  args: {
+    size: 'sm',
+    children: 'Small Button',
+  },
+}
+
+export const Large: Story = {
+  args: {
+    size: 'lg',
+    children: 'Large Button',
+  },
+}
+
+export const Icon: Story = {
+  args: {
+    size: 'icon',
+    children: 'Icon Button',
+  },
+}
