59fc745fb1679cbef48bfe4d93f8ada16896f012
Tsivinsky Petr <creatsiv@gmail.com>
2025-07-15 13:59:00 +0300

    chore: reaplce h3 with div in accordion

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -37,43 +37,45 @@ const AccordionTrigger = React.forwardRef<
   ) => {
     const iconSize = size === 'default' ? 'h-10 w-10 p-2' : 'w-10 h-10 p-[10px]'
     return (
-      <AccordionPrimitive.Header className="flex">
-        <AccordionPrimitive.Trigger
-          ref={ref}
-          className={cn(
-            'flex flex-1 items-center text-left justify-between py1-4 p-4 typo_variant_body transition-all  [&[data-state=open]>svg]:rotate-1801 [&[data-state=open]>.icon-wrapper>.plus]:hidden [&[data-state=open]>.icon-wrapper>.minus]:block text-navy  duration-200 [&[data-state=open]]:bg-navy-opacity-4 rounded-xl hover:bg-navy-opacity-41 border border-transparent hover:border-navy-opacity-16',
-            className
-          )}
-          {...props}
-        >
-          {children && (
-            <span className="text-wrapper transition-all pr-3 leading-none">
-              {children}
-            </span>
-          )}
-          {icons === 'math' && (
-            <span className="relative icon-wrapper">
-              <Plus
-                className={`${iconSize} bg-navy-opacity-4 rounded-lg shrink-0 transition-all duration-200 visible plus`}
-              />
-              <Minus
-                className={`${iconSize} bg-navy-opacity-4 rounded-lg shrink-0 transition-all duration-200 hidden minus`}
-              />
-              {/* <MoreLink16X16 className="h-4 w-4 shrink-0 transition-transform duration-200 visible plus" /> */}
-              {/* <LessLink16X16 className="h-4 w-4 shrink-0 transition-transform duration-200 hidden minus" /> */}
-            </span>
-          )}
-          {icons === 'arrows' && (
-            <span className="relative icon-wrapper">
-              <ArrowDown
-                className={`${iconSize} bg-navy-opacity-4 rounded-lg shrink-0 transition-all duration-200 visible plus`}
-              />
-              <ArrowUp
-                className={`${iconSize} bg-navy-opacity-4 rounded-lg shrink-0 transition-all duration-200 hidden minus`}
-              />
-            </span>
-          )}
-        </AccordionPrimitive.Trigger>
+      <AccordionPrimitive.Header asChild>
+        <div className="flex">
+          <AccordionPrimitive.Trigger
+            ref={ref}
+            className={cn(
+              'flex flex-1 items-center text-left justify-between py1-4 p-4 typo_variant_body transition-all  [&[data-state=open]>svg]:rotate-1801 [&[data-state=open]>.icon-wrapper>.plus]:hidden [&[data-state=open]>.icon-wrapper>.minus]:block text-navy  duration-200 [&[data-state=open]]:bg-navy-opacity-4 rounded-xl hover:bg-navy-opacity-41 border border-transparent hover:border-navy-opacity-16',
+              className
+            )}
+            {...props}
+          >
+            {children && (
+              <span className="text-wrapper transition-all pr-3 leading-none">
+                {children}
+              </span>
+            )}
+            {icons === 'math' && (
+              <span className="relative icon-wrapper">
+                <Plus
+                  className={`${iconSize} bg-navy-opacity-4 rounded-lg shrink-0 transition-all duration-200 visible plus`}
+                />
+                <Minus
+                  className={`${iconSize} bg-navy-opacity-4 rounded-lg shrink-0 transition-all duration-200 hidden minus`}
+                />
+                {/* <MoreLink16X16 className="h-4 w-4 shrink-0 transition-transform duration-200 visible plus" /> */}
+                {/* <LessLink16X16 className="h-4 w-4 shrink-0 transition-transform duration-200 hidden minus" /> */}
+              </span>
+            )}
+            {icons === 'arrows' && (
+              <span className="relative icon-wrapper">
+                <ArrowDown
+                  className={`${iconSize} bg-navy-opacity-4 rounded-lg shrink-0 transition-all duration-200 visible plus`}
+                />
+                <ArrowUp
+                  className={`${iconSize} bg-navy-opacity-4 rounded-lg shrink-0 transition-all duration-200 hidden minus`}
+                />
+              </span>
+            )}
+          </AccordionPrimitive.Trigger>
+        </div>
       </AccordionPrimitive.Header>
     )
   }
